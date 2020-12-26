ea07db6ed527a73645902b26ddfcd1ef7ad0c581
Andrej Novikov <jazzaiman@gmail.com>
2020-12-26 12:52:51 +0200

    feat: content update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  pathPrefix: `/centauri-stake-pool`,
+  pathPrefix: `/candy-pool`,
   siteMetadata: {
-    siteURL: 'https://shroomist.github.io/centauri-stake-pool/', // replace this with your own domain
-    title: 'Shroomist Centauri Stake Pool', // replace it with your own title
-    description: 'Staking Divination',
+    siteURL: 'https://shroomist.github.io/candy-pool/', // replace this with your own domain
+    title: 'CANDY Pool', // replace it with your own title
+    description: 'Sweet Staking Divination',
     author: 'Andrej Novikov', // this one is take, choose another name
     twitter: false, // replace it, or set to 'false' to disable "Tweet this" button
     social: [ // again, replace thse with your own value, or set to empty array to disable them
       // { name: 'twitter', url: 'https://twitter.com/xiaoxinghu' },
-      { name: 'website', url: 'https://shroomist.github.io/centauri-stake-pool/' },
+      { name: 'website', url: 'https://shroomist.github.io/candy-pool/' },
       { name: 'github', url: 'https://github.com/shroomist' },
       { name: 'email', url: 'mailto:jazzaiman@gmail.com' },
     ]
